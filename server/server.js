4465ae6e272e0eacf2f799abcf0ec1f6c9b2a149
Vrajesh-Techtic <158456729+vrajesh-techtic@users.noreply.github.com>
2024-10-30 14:17:38 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,156 @@
+const express = require("express");
+const app = express();
+const { resolve } = require("path");
+// Replace if using a different env file or config
+const env = require("dotenv").config({ path: "../.env" });
+const cors = require("cors");
+console.log("process.env.STATIC_DIR", env);
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: "2020-08-27",
+  appInfo: {
+    // For sample support and debugging, not required for production:
+    name: "stripe-samples/identity/modal",
+    version: "0.0.1",
+    url: "https://github.com/stripe-samples",
+  },
+});
+
+app.use(cors({ allowedHeaders: "*" }));
+app.use(express.static(process.env.STATIC_DIR));
+app.use(
+  express.json({
+    // We need the raw body to verify webhook signatures.
+    // Let's compute it only when hitting the Stripe webhook endpoint.
+    verify: function (req, res, buf) {
+      if (req.originalUrl.startsWith("/webhook")) {
+        req.rawBody = buf.toString();
+      }
+    },
+  })
+);
+
+app.get("/", (req, res) => {
+  const path = resolve(process.env.STATIC_DIR + "/index.html");
+  res.sendFile(path);
+});
+
+app.get("/api/config", (req, res) => {
+  res.send({
+    publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  });
+});
+
+app.post("/create-verification-session", async (req, res) => {
+  try {
+    const verificationSession =
+      await stripe.identity.verificationSessions.create({
+        type: "document",
+        metadata: {
+          user_id: "{{USER_ID}}",
+        },
+        // Additional options for configuring the verification session:
+        // options: {
+        //   document: {
+        //     # Array of strings of allowed identity document types.
+        //     allowed_types: ['driving_license'], # passport | id_card
+        //
+        //     # Collect an ID number and perform an ID number check with the
+        //     # document’s extracted name and date of birth.
+        //     require_id_number: true,
+        //
+        //     # Disable image uploads, identity document images have to be captured
+        //     # using the device’s camera.
+        //     require_live_capture: true,
+        //
+        //     # Capture a face image and perform a selfie check comparing a photo
+        //     # ID and a picture of your user’s face.
+        //     require_matching_selfie: true,
+        //   }
+        // },
+      });
+
+    // Send publishable key and PaymentIntent details to client
+    res.send({
+      client_secret: verificationSession.client_secret,
+    });
+  } catch (e) {
+    console.log(e);
+    return res.status(400).send({
+      error: {
+        message: e.message,
+      },
+    });
+  }
+});
+
+// Expose a endpoint as a webhook handler for asynchronous events.
+// Configure your webhook in the stripe developer dashboard
+// https://dashboard.stripe.com/test/webhooks
+app.post("/webhook", async (req, res) => {
+  let data, eventType;
+
+  // Check if webhook signing is configured.
+  if (process.env.STRIPE_WEBHOOK_SECRET) {
+    // Retrieve the event by verifying the signature using the raw body and secret.
+    let event;
+    let signature = req.headers["stripe-signature"];
+    try {
+      event = stripe.webhooks.constructEvent(
+        req.rawBody,
+        signature,
+        process.env.STRIPE_WEBHOOK_SECRET
+      );
+    } catch (err) {
+      console.log(`⚠️  Webhook signature verification failed.`);
+      return res.sendStatus(400);
+    }
+    data = event.data;
+    eventType = event.type;
+  } else {
+    // Webhook signing is recommended, but if the secret is not configured in `config.js`,
+    // we can retrieve the event data directly from the request body.
+    data = req.body.data;
+    eventType = req.body.type;
+  }
+
+  // Successfully constructed event
+  switch (eventType) {
+    case "identity.verification_session.verified": {
+      // All the verification checks passed
+      const verificationSession = data.object;
+      break;
+    }
+    case "identity.verification_session.requires_input": {
+      // At least one of the verification checks failed
+      const verificationSession = data.object;
+
+      console.log(
+        "Verification check failed: " + verificationSession.last_error.reason
+      );
+
+      // Handle specific failure reasons
+      switch (verificationSession.last_error.code) {
+        case "document_unverified_other": {
+          // The document was invalid
+          break;
+        }
+        case "document_expired": {
+          // The document was expired
+          break;
+        }
+        case "document_type_not_supported": {
+          // document type not supported
+          break;
+        }
+        default: {
+          // ...
+        }
+      }
+    }
+  }
+  res.sendStatus(200);
+});
+
+app.listen(4242, () =>
+  console.log(`Node server listening at http://localhost:4242`)
+);
